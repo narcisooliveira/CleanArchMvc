87d2c4d5edc46a9ad3191dfdc323adbffc235d41
Narciso Oliveira <nrcsfo@gmail.com>
2026-01-27 10:07:41 -0300

    Implementado endpints GET e POST para ações de edição de categoria. Adicionando Razor view com validação de formulário para edição de detalhes de categoria.

--- CleanArchMvc.WebUI/Controllers/CategoriesController.cs
+++ CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -36,4 +36,28 @@ public class CategoriesController : Controller
         }
         return View(category);
     }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id is null) 
+            return NotFound();
+
+        var categoryDto = await _categoryService.GetById(id.Value);
+        if (categoryDto is null) 
+            return NotFound();
+
+        return View(categoryDto);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(CategoryDTO category)
+    {
+        if (ModelState.IsValid)
+        {
+            await _categoryService.Update(category);
+            return RedirectToAction(nameof(Index));
+        }
+        return View(category);
+    }
 }
